df20c2a27bb642f1c1e1e57e5479613675df5ecf
Pirmin Pfeifer <pirmin.pfeifer@gmx.de>
2022-02-20 01:49:41 +0100

    ForceFieldGen enhancements

--- forceFieldTextureCreator/src/ForceFieldGen.java
+++ forceFieldTextureCreator/src/ForceFieldGen.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 public class ForceFieldGen {
     BufferedImage img;
     boolean generated = false;
+    boolean blur = false;
 
     ForceFieldGen(File in) {
         try {
@@ -18,8 +19,20 @@ public class ForceFieldGen {
         }
     }
 
+    void enableBlur(boolean value) {
+        blur = value;
+    }
+
+    void enableBlur() {
+        blur = true;
+    }
+
     void gen() {
         if (generated) return;
+
+        System.out.println("Started Image Generation");
+        final long init = System.currentTimeMillis();;
+        long start = init;
 
         for (int x = 0; x < img.getWidth(); x++) {
             for (int y = 0; y < img.getHeight(); y++) {
@@ -34,10 +47,12 @@ public class ForceFieldGen {
                 }
                 img.setRGB(x, y, dir.toColor().getRGB());
             }
-            System.out.println("Row " + x+ " Completed");
+            System.out.println("Row " + x + " Completed");
         }
 
-
+        long vectorGenTime = System.currentTimeMillis();
+        System.out.println("Finished Vector Gen in " + (vectorGenTime- start) / 1000.0 + " Seconds.");
+        start = System.currentTimeMillis();
         // Clear Empty
         Color neutral = new Color(127, 127, 255);
         for (int x = 0; x < img.getWidth(); x++) {
@@ -47,6 +62,43 @@ public class ForceFieldGen {
                     img.setRGB(x, y, neutral.getRGB());
             }
         }
+
+        long clearETime = System.currentTimeMillis();
+        System.out.println("Cleared Empty value in " + (clearETime - start) + " Millis.");
+        start = System.currentTimeMillis();
+
+        // Clear Blue
+        for (int x = 0; x < img.getWidth(); x++) {
+            for (int y = 0; y < img.getHeight(); y++) {
+                Color current = new Color(img.getRGB(x, y));
+                Color next = new Color(current.getRed(), current.getGreen(), 0);
+                img.setRGB(x, y, next.getRGB());
+            }
+        }
+
+        long clearBTime = System.currentTimeMillis();
+        System.out.println("Cleared Blue value in " + (clearBTime - start) + " Millis.");
+        start = System.currentTimeMillis();
+
+        // Blur
+        if (blur) {
+            BufferedImage blurred = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+
+            for (int x = 0; x < img.getWidth(); x++) {
+                for (int y = 0; y < img.getHeight(); y++) {
+                    blurred.setRGB(x, y, blurredColor(x, y).getRGB());
+                }
+            }
+
+            img = blurred;
+
+            long blurTime = System.currentTimeMillis();
+            System.out.println("Blurred Image in " + (blurTime - start) + " Millis.");
+            start = System.currentTimeMillis();
+        }
+
+        long totalTime = System.currentTimeMillis();
+        System.out.println("Generation finished in " + (totalTime-init) / 1000.0 + " Seconds.");
         generated = true;
     }
 
@@ -87,5 +139,20 @@ public class ForceFieldGen {
         }
 
         return res;
+    }
+
+    private Color blurredColor(int x, int y) {
+        int r = 0, g = 0, b = 0, count = 0;
+
+        for (int ix = Math.max(x - 1, 0); ix < Math.min(x + 2, img.getWidth()); ix++) {
+            for (int iy = Math.max(y - 1, 0); iy < Math.min(y + 2, img.getHeight()); iy++) {
+                Color color = new Color(img.getRGB(x, y));
+                r += color.getRed();
+                g += color.getGreen();
+                b += color.getBlue();
+                count++;
+            }
+        }
+        return new Color(r / count, g / count, b / count);
     }
 }
